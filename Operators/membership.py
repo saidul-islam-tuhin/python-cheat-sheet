e9dc52463da57789fabfd7517304155fcb069c74
saidul-islam <tuhinmd718@gmail.com>
2018-08-05 13:53:56 +0600

    Give example of all membership operator

---
+++ Operators/membership.py
@@ -0,0 +1,14 @@
+
+""" ----------------------- in operator ----------------------------- """
+'lo' in 'hello' # OUTPUT: True
+'ol' in 'hello' # OUTPUT: False
+2 in [1,2] # OUTPUT: True
+
+""" ----------------------- in operator(case sensitive) ----------------------------- """
+'hello' in 'hello' # OUTPUT: True
+'Hello' in 'hello' # OUTPUT: False
+
+
+""" ----------------------- not in operator ----------------------------- """
+3 not in [1,2] # Output: True
+'o' not in 'hello' # OUTPUT: False
