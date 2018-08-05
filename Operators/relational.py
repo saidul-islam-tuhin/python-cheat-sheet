98035b8640e769ac5cd2212485099e337d20a1e8
saidul-islam <tuhinmd718@gmail.com>
2018-08-05 13:54:20 +0600

    Give example of all relational operator

---
+++ Operators/relational.py
@@ -0,0 +1,31 @@
+
+""" ----------------------- Equal To ----------------------------- """
+1 == 1 # OUTPUT: True
+1 == 2 # OUTPUT: False
+1 == '1' # OUTPUT: False
+'same' == 'same' # OUTPUT: True
+
+""" ----------------------- Less than ----------------------------- """
+2 < 3 # OUTPUT: True
+2 < 2 # OUTPUT: False
+2 < 1 # OUTPUT: False
+
+""" ----------------------- Less than equal ----------------------------- """
+2 <= 3 # OUTPUT: True
+2 <= 2 # OUTPUT: True
+2 <= 1 # OUTPUT: False
+
+""" ----------------------- Greater than ----------------------------- """
+4 > 3 # OUTPUT: True
+4 > 4 # OUTPUT: False
+4 > 5 # OUTPUT: False
+
+""" ----------------------- Greater than equal ----------------------------- """
+4 >= 3 # OUTPUT: True
+4 >= 4 # OUTPUT: True
+4 >= 5 # OUTPUT: False
+
+""" ----------------------- Not equal ----------------------------- """
+1 != 1 # OUTPUT: False
+1 != 2 # OUTPUT: True
+1 != '1' # OUTPUT: True
