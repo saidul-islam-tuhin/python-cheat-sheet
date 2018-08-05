8571b26cc5e24b93e0dd12900fb5a6da45dd5590
saidul-islam <tuhinmd718@gmail.com>
2018-08-05 13:49:40 +0600

    Give example of string module
    Module: ascii_letters, ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits, printable, punctuation, whitespace

---
+++ String/string_module.py
@@ -0,0 +1,24 @@
+"""
+Module: ascii_letters, ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits,
+        printable, punctuation, whitespace
+"""
+
+import string
+
+string.ascii_letters # OUTPUT: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+string.ascii_lowercase # OUTPUT: 'abcdefghijklmnopqrstuvwxyz'
+
+string.ascii_uppercase # OUTPUT: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+string.digits # OUTPUT :'0123456789'
+
+string.hexdigits # OUTPUT :'0123456789abcdefABCDEF'
+
+string.octdigits # OUTPUT :'01234567'
+
+string.printable # OUTPUT: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c
+
+string.punctuation # OUTPUT: '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+
+string.whitespace # OUTPYT:  ' \t\n\r\x0b\x0c'
