563a894d605847a343e8e2d93e4ee6692bb7dca9
saidul-islam <tuhinmd718@gmail.com>
2018-09-18 12:14:45 +0600

    Removed print()

--- String/string_method.py
+++ String/string_method.py
@@ -5,27 +5,28 @@ Method: capitalize, casefold, lower, title, upper, center, encode
 
 example = 'an example for capitalize()'
 print(example) # OUTPUT: an example for capitalize()
-print(example.capitalize()) # OUTPUT: An example for capitalize()
+example.capitalize() # OUTPUT: An example for capitalize()
 
 casefold_exmp = 'Sample String'
 print(casefold_exmp) # OUTPUT: Sample String
-print(casefold_exmp.casefold()) # OUTPUT: sample string
+casefold_exmp.casefold() # OUTPUT: sample string
 
 print(casefold_exmp) # OUTPUT: Sample String
-print(casefold_exmp.lower()) # OUTPUT: sample string
+casefold_exmp.lower() # OUTPUT: sample string
+
 
 """----------------- casefold() vs lower() ----------------"""
 a = 'Fluß'
-print(a.lower()) # OUTPUT: fluß
-print(a.casefold()) # OUTPUT: fluss
+a.lower() # OUTPUT: fluß
+a.casefold() # OUTPUT: fluss
+
 
 name = 'md. sadiul islam tuhin'
 print(name) # OUTPUT: md. sadiul islam tuhin
-print(name.title()) # OUTPUT: Md. Sadiul Islam Tuhin
 
+name.title() # OUTPUT: Md. Sadiul Islam Tuhin
 
-print(name) # OUTPUT: md. sadiul islam tuhin
-print(name.upper()) # OUTPUT: MD. SADIUL ISLAM TUHIN
+name.upper() # OUTPUT: MD. SADIUL ISLAM TUHIN
 
 center_exmp = 'An example of center()'
 print(center_exmp) # OUTPUT: An example of center()
@@ -37,9 +38,48 @@ encode_exmp = '$encöde!'
 print(encode_exmp) # OUTPUT: $encöde!
 
 # Default encode to utf-8
-print(encode_exmp.encode()) # OUTPUT: '$enc\xc3\xb6de!'
+encode_exmp.encode() # OUTPUT: '$enc\xc3\xb6de!'
       
-print(encode_exmp.encode('ascii')) # UnicodeEncodeError: 'ascii' codec can't encode character '\xf6' in position 4
-print(encode_exmp.encode('ascii','ignore')) # OUTPUT: b'$encde!'
-print(encode_exmp.encode('ascii','replace')) # OUTPUT: b'$enc?de!'
-print(encode_exmp.encode('ascii','backslashreplace')) # OUTPUT: b'$enc\\xf6de!'
+encode_exmp.encode('ascii') # UnicodeEncodeError: 'ascii' codec can't encode character '\xf6' in position 4
+encode_exmp.encode('ascii','ignore') # OUTPUT: b'$encde!'
+encode_exmp.encode('ascii','replace') # OUTPUT: b'$enc?de!'
+encode_exmp.encode('ascii','backslashreplace') # OUTPUT: b'$enc\\xf6de!'
+
+
+
+test = 'a sample string.'
+
+# string.index(substring, start, end)
+# If sunstring found then return result otherwise raise ValueError
+test.index('sam') # OUTPUT: 2
+test.index('smple') # ValueError: substring not found
+
+test.index('sam', 1) # OUTPUT: 2
+test.index('sam', 3) # ValueError: substring not found
+
+test.index('ple', 3, 8) # OUTPUT: 5
+test.index('ple', 3, 5) # ValueError: substring not found
+
+
+test.startswith('a') # OUTPUT: True
+test.startswith('A') # OUTPUT: False
+
+test.startswith('s', 2) # OUTPUT: True
+test.startswith('s', 0) # OUTPUT: False
+
+test.startswith('sam', 2, 7) # OUTPUT: True
+
+
+
+test.endswith('string.') # OUTPUT: True
+test.endswith('string') # OUTPUT: False
+
+test.endswith('string.', 9) # OUTPUT: True
+test.endswith('string.', 10) # OUTPUT: False
+
+test.endswith('str',9,12) # OUTPUT: True
+test.endswith('str',9,14) # OUTPUT: False
+
+
+
+
